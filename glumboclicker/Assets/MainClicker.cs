54c3ad9d6c13e195c0ee183fed737482aafe3a9b
Ghastlymicrowave <supervaughanbros@gmail.com>
2022-05-01 17:01:19 -0400

    URP

--- glumboclicker/Assets/MainClicker.cs
+++ glumboclicker/Assets/MainClicker.cs
@@ -7,6 +7,7 @@ public class MainClicker : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] Camera mainCam;
     [SerializeField] GlumboCoin coin;
+    [SerializeField] PhoneClick phone;
     void Start()
     {
         
@@ -15,16 +16,41 @@ public class MainClicker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     if(Input.GetMouseButtonDown(0)){
-         
-        Ray camRay = mainCam.ScreenPointToRay(new Vector3(Input.mousePosition.x,Input.mousePosition.y,0f));
-        RaycastHit hitRay;
-        Physics.Raycast(camRay, out hitRay);
-        if(hitRay.collider!=null){
-            if(hitRay.collider.tag=="Glumbocoin")
-                coin.Spin();
-            }   
-        }        
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray camRay = mainCam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+            RaycastHit hitRay;
+            Physics.Raycast(camRay, out hitRay);
+            if (hitRay.collider != null)
+            {
+                switch (hitRay.collider.tag)
+                {
+                    case "Glumbocoin":
+                        coin.Spin();
+                        break;
+                    default: break;
+                }
+            }
+        }
+        else
+        {
+            Ray camRay = mainCam.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+            RaycastHit hitRay;
+            Physics.Raycast(camRay, out hitRay);
+            if (hitRay.collider != null)
+            {
+                switch (hitRay.collider.tag)
+                {
+                    case "Glumbocoin":
+                        //glow the coin?
+                        break;
+                    case "Phone":
+                        phone.MouseHover(hitRay.point);
+                        break;
+                    default: break;
+                }
+            }
+        }
     }
-    
 }
